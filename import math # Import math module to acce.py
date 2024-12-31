0ec9d335df6e9fc67d0b82bd8f9531211e136063
Omarhussien777 <omar.hussien@meti.dev>
2024-12-31 17:13:43 +0200

    Add files via upload

---
+++ import math # Import math module to acce.py
@@ -0,0 +1,13 @@
+
+# Define the radius of the circle
+radius = 5  # You can change this value to any other value
+
+# Calculate the circumference
+circumference = 2 * 3.14159 * radius  # Using the formula 2 * π * r
+
+# Calculate the area
+area = 3.14159 * (radius ** 2)  # Using the formula π * r^2
+
+# Print the variables
+print("Circumference:", circumference)
+print("Area:", area)
